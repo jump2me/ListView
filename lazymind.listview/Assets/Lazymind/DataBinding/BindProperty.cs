54e9a4622e46de150e945debcd192c8e720528fd
lazymind <eunku.lee@gmail.com>
2017-12-10 14:57:52 +0900

    BindProierty
    BindProierty

---
+++ lazymind.listview/Assets/Lazymind/DataBinding/BindProperty.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindProperty<TYPE>
+{
+    public TYPE m_value;
+    public TYPE Value
+    {
+        get
+        {
+            return m_value;
+        }
+        set
+        {
+            m_value = value;
+
+            OnValueChanged();
+        }
+    }
+
+    public delegate void ValueChangeEvent();
+    private event ValueChangeEvent ValueChanged;
+    private void OnValueChanged()
+    {
+        if (ValueChanged != null)
+        {
+            ValueChanged();
+        }
+    }
+
+    public BindProperty(ValueChangeEvent _callback)
+    {
+        ValueChanged += _callback;
+    }
+}
--- lazymind.listview/Assets/Lazymind/ListView/Scripts/ListView/ListViewData.cs
+++ lazymind.listview/Assets/Lazymind/ListView/Scripts/ListView/ListViewData.cs
@@ -1,39 +1,6 @@
 ﻿using UnityEngine;
 using Lazymind;
 
-public class BindProperty<TYPE>
-{
-    public TYPE m_value;
-    public TYPE Value
-    {
-        get
-        {
-            return m_value;
-        }
-        set
-        {
-            m_value = value;
-
-            OnValueChanged();
-        }
-    }
-
-    public delegate void ValueChangeEvent();
-    private event ValueChangeEvent ValueChanged;
-    private void OnValueChanged()
-    {
-        if (ValueChanged != null)
-        {
-            ValueChanged();
-        }
-    }
-
-    public BindProperty(ValueChangeEvent _callback)
-    {
-        ValueChanged += _callback;
-    }
-}
-
 public class ListViewData
 { 
     public int ID { get; private set; }
